54c97501a58cea0666e36362902649c3e174ade5
Tin Vukšan <tin.vuksan@vuv.hr>
2022-08-01 20:37:33 +0200

    step-19

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -5,6 +5,7 @@ import 'auth_user.dart';
 class AuthService implements AuthProvider {
   final AuthProvider provider;
   const AuthService(this.provider);
+
   factory AuthService.firebase() => AuthService(FirebaseAuthProvider());
   @override
   Future<AuthUser> createUser({
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -111,6 +111,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'my_id',
       email: 'tin.vuksan@vuv.hr',
       isEmailVerified: false,
     );
@@ -132,6 +133,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: 'my_id',
       email: 'tin.vuksan@vuv.hr',
       isEmailVerified: true,
     );
