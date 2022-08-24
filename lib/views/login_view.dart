175ddaca1afbdce5f8cc781f247c72499eb5f257
Tin Vukšan <tin.vuksan@vuv.hr>
2022-08-24 17:59:44 +0200

    step-24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -4,6 +4,8 @@ import 'package:mynotes/constants/routes.dart';
 import 'package:mynotes/services/auth/auth_exceptions.dart';
 import 'package:mynotes/services/auth/bloc/auth_bloc.dart';
 import 'package:mynotes/services/auth/bloc/auth_event.dart';
+
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
 import '../utilities/dialogs/error_dialog.dart';
 
 class LoginView extends StatefulWidget {
@@ -37,68 +39,60 @@ class _LoginViewState extends State<LoginView> {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Column(
-        children: [
-          TextField(
-            enableSuggestions: false,
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            controller: _email,
-            decoration: const InputDecoration(
-              hintText: 'Enter your email here',
-            ),
+      body: Column(children: [
+        TextField(
+          enableSuggestions: false,
+          autocorrect: false,
+          keyboardType: TextInputType.emailAddress,
+          controller: _email,
+          decoration: const InputDecoration(
+            hintText: 'Enter your email here',
           ),
-          TextField(
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-            controller: _password,
-            decoration: const InputDecoration(
-              hintText: 'Enter your password here',
-            ),
+        ),
+        TextField(
+          obscureText: true,
+          enableSuggestions: false,
+          autocorrect: false,
+          controller: _password,
+          decoration: const InputDecoration(
+            hintText: 'Enter your password here',
           ),
-          TextButton(
+        ),
+        BlocListener<AuthBloc, AuthState>(
+          listener: (context, state) async {
+            if (state is AuthStateLoggedOut) {
+              if (state.exception is UserNotFoundAuthException ||
+                  state.exception is WrongPasswordAuthException) {
+                await showErrorDialog(context, "Wrong credentials");
+              } else if (state.exception is GenericAuthException) {
+                await showErrorDialog(context, "Authentication error");
+              }
+            }
+          },
+          child: TextButton(
             onPressed: () async {
               final email = _email.text;
               final password = _password.text;
-
-              try {
-                context.read<AuthBloc>().add(
-                      AuthEventLogIn(
-                        email,
-                        password,
-                      ),
-                    );
-              } on UserNotFoundAuthException {
-                await showErrorDialog(
-                  context,
-                  'User not found',
-                );
-              } on WrongPasswordAuthException {
-                await showErrorDialog(
-                  context,
-                  'Wrong password',
-                );
-              } on GenericAuthException {
-                await showErrorDialog(
-                  context,
-                  'Authentication error',
-                );
-              }
+              context.read<AuthBloc>().add(
+                    AuthEventLogIn(
+                      email,
+                      password,
+                    ),
+                  );
             },
             child: const Text('Login'),
           ),
-          TextButton(
-            onPressed: () => {
-              Navigator.of(context).pushNamedAndRemoveUntil(
-                registerRoute,
-                (route) => false,
-              )
-            },
-            child: const Text("Not registered yet? Register here!"),
-          ),
-        ],
-      ),
+        ),
+        TextButton(
+          onPressed: () => {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+              registerRoute,
+              (route) => false,
+            )
+          },
+          child: const Text("Not registered yet? Register here!"),
+        ),
+      ]),
     );
   }
 }
